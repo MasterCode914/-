0a981bba60c257e9330979e80e394fda81dab3e0
米咔丘 <76833272+MasterCode914@users.noreply.github.com>
2024-05-27 00:24:41 +0800

    Add files via upload

--- Headers/gamepanel.h
+++ Headers/gamepanel.h
@@ -5,6 +5,7 @@
 #include "gamecontrol.h"
 #include <QMap>
 
+#include <QLabel>
 #include <QMainWindow>
 
 QT_BEGIN_NAMESPACE
@@ -27,13 +28,44 @@ public:
 
     // 切割并存储图片
     void initCardMap();
+
     // 剪裁图片
     void cropImage(QPixmap& pix, int x, int y, Card& c);
+
+    // 初始化游戏中的按钮组
+    void initButtonGroup();
+
+    // 初始化游戏中的上下文环境
+    void initPlayerContext();
 
 
 protected:
     void paintEvent(QPaintEvent* ev);           // 重写父类函数绘制图像
 private:
+    enum CardAlign {
+        Horizontal,
+        Vertical
+    };
+    // 保存上下文环境信息
+    struct PlayerContext
+    {
+        // 1. 玩家扑克牌的显示区域
+        QRect cardRect;
+        // 2. 出牌的区域
+        QRect playHandRect;
+        // 3. 扑克牌的对齐方式（水平/垂直）
+        CardAlign align;
+        // 4. 扑克牌显示正面还是背面
+        bool isFrontSide;
+        // 5. 游戏过程中的提示信息，例如：不出
+        QLabel* info;
+        // 6. 玩家的头像
+        QLabel* roleImg;
+        // 7. 玩家刚打出的牌
+        Cards lastCards;
+    };
+    QMap<Player*, PlayerContext> m_contextMap;
+
     Ui::GamePanel *ui;
     QPixmap m_bkImage;      // 背景图片
     GameControl* m_gameCtl;
