5b66040424aea69ee809d1b2aeaa63f0ece0f608
米咔丘 <76833272+MasterCode914@users.noreply.github.com>
2024-05-26 18:41:18 +0800

    Add files via upload

--- Sources/gamepanel.cpp
+++ Sources/gamepanel.cpp
@@ -23,10 +23,13 @@ GamePanel::GamePanel(QWidget *parent)
 
 
     // 3. 实例化控制游戏类的对象
+    gameControlInit();
 
     // 4. 玩家得分（更新）
+    updatePlayerScore();
 
     // 5. 切割游戏图片
+    initCardMap();
 
     // 6. 初始化游戏中的按钮组
 
@@ -37,6 +40,69 @@ GamePanel::GamePanel(QWidget *parent)
 GamePanel::~GamePanel()
 {
     delete ui;
+}
+
+void GamePanel::gameControlInit()
+{
+    m_gameCtl = new GameControl(this);
+    m_gameCtl->playInit();
+    // 得到三个玩家的实例对象
+    Robot* leftRobot = m_gameCtl->getLeftRobot();
+    Robot* rightRobot = m_gameCtl->getRightRobot();
+    UserPlayer* user = m_gameCtl->getUserPlayer();
+    // 存储的顺序：左侧机器人  右侧机器人   玩家
+    m_playerList << leftRobot << rightRobot << user;
+
+}
+
+void GamePanel::updatePlayerScore()
+{
+    ui->scorePanel->setScores(
+                m_playerList[0]->getScore(),
+            m_playerList[1]->getScore(),
+            m_playerList[2]->getScore());
+}
+
+void GamePanel::initCardMap()
+{
+    // 1. 加载需要剪裁的大图
+    QPixmap pixmap(":/images/card.png");
+    // 2. 计算每张卡牌的大小
+    m_cardSize.setWidth(pixmap.width() / 13);
+    m_cardSize.setHeight(pixmap.height() / 5);
+
+    // 3. 背景图（位于大图最后面）
+    // pixmap的copy可以实现对图片的剪裁并返回pixmap
+    m_cardBackImg = pixmap.copy(2 * m_cardSize.width(), 4 * m_cardSize.height(),
+                                m_cardSize.width(), m_cardSize.height());
+
+    // 4. 剪裁正常花色卡牌添加到容器中
+    for (int i= 0, suit = Card::Suite_Begin + 1; suit < Card::Suite_End; ++suit, ++i) {
+        for (int j = 0, pt = Card::Card_Begin + 1; pt < Card::Card_Begin; ++pt, ++j) {
+            Card card((Card::CardPoint)pt, (Card::CardSuit)suit);
+            cropImage(pixmap, j * m_cardSize.width(), i * m_cardSize.height(), card);
+        }
+    }
+
+    // 大小王
+    Card c;
+    c.setPoint(Card::Card_SJ);
+    c.setSuit(Card::Suite_Begin);
+    cropImage(pixmap, 0, 4 * m_cardSize.height(), c);
+
+    c.setPoint(Card::Card_BJ);
+    cropImage(pixmap, m_cardSize.width(), 4 * m_cardSize.height(), c);
+
+}
+
+void GamePanel::cropImage(QPixmap &pix, int x, int y, Card &c)
+{
+    QPixmap sub = pix.copy(x, y, m_cardSize.width(), m_cardSize.height());
+    CardPanel* panel = new CardPanel(this);
+    panel->setCard(c);      // 设置卡牌
+    panel->setImage(sub, m_cardBackImg);        // 设置前景图和背景图
+    panel->hide();          // 暂时需要隐藏卡牌窗口
+    m_cardMap.insert(c, panel);
 }
 
 void GamePanel::paintEvent(QPaintEvent *ev)
